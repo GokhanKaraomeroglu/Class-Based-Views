7b076e52e1ccc7df3f8d1b9c56ee01c26f6a52fa
Himmel König <itogreniyorum@gmail.com>
2021-10-21 22:47:26 +0300

    ValidationError added

--- fscohort/forms.py
+++ fscohort/forms.py
@@ -1,9 +1,18 @@
 from django import  forms
 from django.forms import fields
 from .models import Student
+from django.core.exceptions import ValidationError
 
 
 class StudentForm(forms.ModelForm):
+    def clean_number(self):
+        number = self.cleaned_data['number']
+        if not (1000 < number < 1000):
+            raise ValidationError ('Student Number is not valid')
+        return number
+        
+    
+    
     class Meta:
         model = Student
         fields = "__all__"
